70c2513926c40b6a1b1215456f372339e82f755f
Egor Grischenkov <66774252+egrischenkov@users.noreply.github.com>
2025-05-01 15:59:55 +0300

    feat(init): setup directory structure

---
+++ android/app/src/main/kotlin/com/example/taro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.taro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Taro());
+}
+
+class Taro extends StatelessWidget {
+  const Taro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
