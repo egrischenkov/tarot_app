54b129979014c7c599789cb1bbd377b4f0534e2f
Egor Grischenkov <66774252+egrischenkov@users.noreply.github.com>
2025-05-02 21:17:25 +0300

    refactor(deps): wrap app with global dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:taro/core/assets/gen/assets.gen.dart';
+import 'package:taro/core/di/app_dependencies_container.dart';
+import 'package:taro/core/di/app_dependencies_scope.dart';
 import 'package:taro/core/localization/gen/app_localizations.g.dart';
 
 void main() {
@@ -11,10 +13,13 @@ class Taro extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: Center(child: Assets.tarotMeets.image(width: 200, height: 200, fit: BoxFit.cover)),
+    return AppDependenciesScope(
+      dependencies: const AppDependenciesContainer(),
+      child: MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: Center(child: Assets.tarotMeets.image(width: 200, height: 200, fit: BoxFit.cover)),
+      ),
     );
   }
 }
