48e327ec67fd721ccc9affe42eac1258c50a7b41
Egor Grischenkov <66774252+egrischenkov@users.noreply.github.com>
2025-07-12 10:19:42 +0300

    TAROT-24: feat(ui-kit): add new make command for updating goldens

---
+++ scripts/update_goldens.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+test_dirs=()
+while IFS= read -r dir; do
+  if [ -d "$dir/test" ]; then
+    test_dirs+=("$dir")
+  fi
+done < <(find . -type f -name "pubspec.yaml" -exec dirname {} \;)
+
+if [ ${#test_dirs[@]} -gt 0 ]; then
+  fvm flutter test --update-goldens "${test_dirs[@]}" --no-pub 
+else
+  echo "No directories with pubspec.yaml and a test/ folder found."
+fi
