a00a0d86ad78b2c068265417ed7c8e011489d03a
Egor Grischenkov <66774252+egrischenkov@users.noreply.github.com>
2025-07-24 21:29:15 +0300

    TAROT-31: feat(home screen): make base layout for home screen

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,10 +1,8 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:taro/core/assets/gen/assets.gen.dart';
-import 'package:taro/core/di/app_dependencies_scope.dart';
-import 'package:tarot_analytics/scr/analytics_reporter.dart';
+import 'package:tarot_ui_kit/ui_kit.dart';
+
+const _appBarHeight = 120.0;
 
 @RoutePage()
 class HomeScreen extends StatefulWidget {
@@ -15,114 +13,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-  bool _isRotating = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 4),
-      vsync: this,
-    )..repeat();
-    _controller.stop();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _toggleRotation(AnalyticsReporter analyticsReporter) {
-    analyticsReporter.logEvent(
-      const AnalyticsEvent('haptic_event'),
-    );
-    HapticFeedback.mediumImpact();
-
-    setState(() {
-      if (_isRotating) {
-        _controller.stop();
-      } else {
-        _controller.repeat();
-      }
-      _isRotating = !_isRotating;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final analyticsReporter = AppDependenciesScope.of(context).analyticsReporter;
+    final colors = context.colors;
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RotationTransition(
-              turns: _controller,
-              child: Assets.tarotMeets.image(
-                width: 200,
-                height: 200,
-                fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              height: _appBarHeight,
+              color: colors.whiteBgWhite,
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [],
               ),
             ),
-            const SizedBox(height: 100),
-            InkWell(
-              onTap: () => _toggleRotation(analyticsReporter),
-              borderRadius: BorderRadius.circular(8),
-              child: Ink(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.yellow,
-                ),
-                child: Text(
-                  _isRotating ? 'Стоп' : 'Закружило голову хмельную',
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            InkWell(
-              onTap: () {
-                analyticsReporter.logEvent(
-                  const AnalyticsEvent('test_event'),
-                );
-                HapticFeedback.lightImpact();
-              },
-              borderRadius: BorderRadius.circular(8),
-              child: Ink(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.yellow,
-                ),
-                child: const Text('Отправить тестовое событие в Firebase'),
-              ),
-            ),
-            const SizedBox(height: 16),
-            InkWell(
-              onTap: () {
-                FirebaseCrashlytics.instance.crash();
-                HapticFeedback.lightImpact();
-              },
-              borderRadius: BorderRadius.circular(8),
-              child: Ink(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.yellow,
-                ),
-                child: const Text('Тестовый краш в Firebase'),
-              ),
-            ),
-          ],
-        ),
+          ),
+          const Placeholder(),
+        ],
       ),
     );
   }
