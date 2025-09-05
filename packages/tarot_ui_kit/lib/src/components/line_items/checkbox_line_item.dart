bae8d3f8d80c9414a1ea4a5fa9e61d45337397de
Egor Grischenkov <66774252+egrischenkov@users.noreply.github.com>
2025-09-05 20:08:12 +0300

    TAROT-73: feat(ui-kit): add checkbox to ui kit

---
+++ packages/tarot_ui_kit/lib/src/components/line_items/checkbox_line_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:tarot_ui_kit/ui_kit.dart';
+
+class UiKitCheckboxLineItem extends StatelessWidget {
+  final String title;
+  final ValueSetter<bool?> onChanged;
+  final bool isSelected;
+
+  const UiKitCheckboxLineItem({
+    required this.title,
+    required this.onChanged,
+    required this.isSelected,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.colors;
+    final fonts = context.fonts;
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: colors.whiteBgWhite,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: fonts.bodyRegular,
+          ),
+          UiKitSpacing.x2.w,
+          Checkbox(
+            value: isSelected,
+            fillColor: WidgetStateProperty.resolveWith((states) {
+              if (!states.contains(WidgetState.selected)) {
+                return colors.whiteBgWhite;
+              }
+              return null;
+            }),
+            side: BorderSide(color: colors.grey),
+            onChanged: onChanged,
+            shape: const CircleBorder(),
+            checkColor: colors.whiteBgWhite,
+            activeColor: colors.accentYellow,
+          ),
+        ],
+      ),
+    );
+  }
+}
