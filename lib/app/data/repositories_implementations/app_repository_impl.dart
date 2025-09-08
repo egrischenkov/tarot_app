c159d72ecbd9b98653aa4a9b3d6eb8dd1a620220
Egor Grischenkov <66774252+egrischenkov@users.noreply.github.com>
2025-09-08 18:38:13 +0300

    TAROT-73: feat(language): implement app repository

--- lib/app/data/repositories_implementations/app_repository_impl.dart
+++ lib/app/data/repositories_implementations/app_repository_impl.dart
@@ -1,15 +1,69 @@
-import 'package:taro/app/data/models/language_model.dart';
+import 'dart:ui';
+
 import 'package:taro/app/data/models/theme_model.dart';
+import 'package:taro/app/data/services/language_service.dart';
 import 'package:taro/app/domain/entities/language_option.dart';
 import 'package:taro/app/domain/entities/theme_option.dart';
+import 'package:taro/app/domain/repositories/app_repository.dart';
 
-class AppRepositoryImpl {}
+class AppRepositoryImpl implements AppRepository {
+  static const _russianLanguageCode = 'ru';
+  static const _englishLanguageCode = 'en';
+  static const _germanLanguageCode = 'de';
+  static const _spanishLanguageCode = 'es';
+  static const _frenchLanguageCode = 'fr';
+  static const _hindiLanguageCode = 'hi';
+  static const _italianLanguageCode = 'it';
+  static const _portugueseLanguageCode = 'pt';
+  static const _chineseLanguageCode = 'zh';
 
-extension on LanguageModel {
+  final LocaleService _localeService;
+
+  AppRepositoryImpl({
+    required LocaleService localeService,
+  }) : _localeService = localeService;
+
+  @override
+  LanguageOption getCurrentLanguageOption() {
+    return _localeService.currentLocale._toLanguageOption;
+  }
+
+  @override
+  Future<void> setCurrentLanguage({required LanguageOption languageOption}) {
+    return _localeService.selectNewLocale(languageOption.toLocale);
+  }
+}
+
+extension LanguageOptionX on LanguageOption {
+  Locale get toLocale {
+    return switch (this) {
+      LanguageOption.russian => const Locale(AppRepositoryImpl._russianLanguageCode),
+      LanguageOption.english => const Locale(AppRepositoryImpl._englishLanguageCode),
+      LanguageOption.german => const Locale(AppRepositoryImpl._germanLanguageCode),
+      LanguageOption.spanish => const Locale(AppRepositoryImpl._spanishLanguageCode),
+      LanguageOption.french => const Locale(AppRepositoryImpl._frenchLanguageCode),
+      LanguageOption.hindi => const Locale(AppRepositoryImpl._hindiLanguageCode),
+      LanguageOption.italian => const Locale(AppRepositoryImpl._italianLanguageCode),
+      LanguageOption.portuguese => const Locale(AppRepositoryImpl._portugueseLanguageCode),
+      LanguageOption.chinese => const Locale(AppRepositoryImpl._chineseLanguageCode),
+    };
+  }
+}
+
+extension on Locale {
   LanguageOption get _toLanguageOption {
-    return LanguageOption.values.singleWhere((value) {
-      return value.name == name;
-    });
+    return switch (languageCode) {
+      AppRepositoryImpl._russianLanguageCode => LanguageOption.russian,
+      AppRepositoryImpl._englishLanguageCode => LanguageOption.english,
+      AppRepositoryImpl._germanLanguageCode => LanguageOption.german,
+      AppRepositoryImpl._spanishLanguageCode => LanguageOption.spanish,
+      AppRepositoryImpl._frenchLanguageCode => LanguageOption.french,
+      AppRepositoryImpl._hindiLanguageCode => LanguageOption.hindi,
+      AppRepositoryImpl._italianLanguageCode => LanguageOption.italian,
+      AppRepositoryImpl._portugueseLanguageCode => LanguageOption.portuguese,
+      AppRepositoryImpl._chineseLanguageCode => LanguageOption.chinese,
+      _ => LanguageOption.english,
+    };
   }
 }
 
--- lib/app/domain/repositories/app_repository.dart
+++ lib/app/domain/repositories/app_repository.dart
@@ -1,1 +1,7 @@
-class AppRepository {}
+import 'package:taro/app/domain/entities/language_option.dart';
+
+abstract interface class AppRepository {
+  LanguageOption getCurrentLanguageOption();
+
+  Future<void> setCurrentLanguage({required LanguageOption languageOption});
+}
