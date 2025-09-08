b7066951da88836e6a7376ab1ac49ff7f48b40f7
Egor Grischenkov <66774252+egrischenkov@users.noreply.github.com>
2025-09-08 18:38:32 +0300

    TAROT-73: feat(language): implement change language use case

--- lib/app/domain/use_cases/change_language_use_case.dart
+++ lib/app/domain/use_cases/change_language_use_case.dart
@@ -1,1 +1,14 @@
+import 'package:taro/app/domain/entities/language_option.dart';
+import 'package:taro/app/domain/repositories/app_repository.dart';
 
+class ChangeLanguageUseCase {
+  final AppRepository _repository;
+
+  ChangeLanguageUseCase({
+    required AppRepository repository,
+  }) : _repository = repository;
+
+  Future<void> changeLanguage({required LanguageOption languageOption}) {
+    return _repository.setCurrentLanguage(languageOption: languageOption);
+  }
+}
