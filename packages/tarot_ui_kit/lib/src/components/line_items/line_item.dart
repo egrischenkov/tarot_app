f76819fe2645079a0fa546b8361fdad200eb1180
Egor Grischenkov <66774252+egrischenkov@users.noreply.github.com>
2025-09-09 12:24:38 +0300

    TAROT-73: refactor(ui-kit): add value label for line item

--- packages/tarot_ui_kit/lib/src/components/line_items/line_item.dart
+++ packages/tarot_ui_kit/lib/src/components/line_items/line_item.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:tarot_ui_kit/ui_kit.dart';
 
 class UiKitLineItem extends StatelessWidget {
-  final String title;
+  final String label;
   final VoidCallback onTap;
+  final String? valueLabel;
 
   const UiKitLineItem({
-    required this.title,
+    required this.label,
     required this.onTap,
+    this.valueLabel,
     super.key,
   });
 
@@ -15,6 +17,8 @@ class UiKitLineItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final colors = context.colors;
     final fonts = context.fonts;
+
+    final localValueLabel = valueLabel;
 
     return GestureDetector(
       onTap: onTap,
@@ -26,12 +30,21 @@ class UiKitLineItem extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
-              title,
+              label,
               style: fonts.bodyRegular,
+              overflow: TextOverflow.ellipsis,
             ),
-            const Icon(
-              Icons.arrow_forward_ios,
-              size: UiKitSize.x4,
+            Row(
+              children: [
+                if (localValueLabel != null) ...[
+                  Text(localValueLabel, style: fonts.xsLabel),
+                  UiKitSpacing.x4.w,
+                ],
+                const Icon(
+                  Icons.arrow_forward_ios,
+                  size: UiKitSize.x4,
+                ),
+              ],
             ),
           ],
         ),
