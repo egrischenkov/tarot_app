65d94fe145f52d81bbee32f2cab5e172e3d5ee9d
Egor Grischenkov <66774252+egrischenkov@users.noreply.github.com>
2025-09-26 13:04:20 +0300

    TAROT-76: tests(colors): add tests for dark theme

--- packages/tarot_ui_kit/test/src/theme/theme_extensions/colors_golden_test.dart
+++ packages/tarot_ui_kit/test/src/theme/theme_extensions/colors_golden_test.dart
@@ -6,8 +6,8 @@ import 'package:tarot_ui_kit/ui_kit.dart';
 void main() {
   group('UiKitTheme Colors Golden Tests', () {
     goldenTest(
-      'Theme colors palette',
-      fileName: 'theme_colors',
+      'Theme colors palette light mode',
+      fileName: 'theme_colors_light_mode',
       builder: () => GoldenTestGroup(
         children: [
           GoldenTestScenario(
@@ -140,6 +140,219 @@ void main() {
             name: 'Link',
             child: _ColorTile(
               color: UiKitTheme.lightThemeColors.link,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Onboarding-Background-1',
+            child: _ColorTile(
+              color: UiKitTheme.lightThemeColors.onboardingBackground1,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Onboarding-Background-2',
+            child: _ColorTile(
+              color: UiKitTheme.lightThemeColors.onboardingBackground2,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Onboarding-Background-3',
+            child: _ColorTile(
+              color: UiKitTheme.lightThemeColors.onboardingBackground3,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Menu-Card-Gradient-First',
+            child: _ColorTile(
+              color: UiKitTheme.lightThemeColors.menuCardGradientFirst,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Menu-Card-Gradient-Second',
+            child: _ColorTile(
+              color: UiKitTheme.lightThemeColors.menuCardGradientSecond,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Menu-Card-Content',
+            child: _ColorTile(
+              color: UiKitTheme.lightThemeColors.menuCardContent,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    goldenTest(
+      'Theme colors palette dark mode',
+      fileName: 'theme_colors_dark_mode',
+      builder: () => GoldenTestGroup(
+        children: [
+          GoldenTestScenario(
+            name: 'Accent',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.accent,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Accent-Secondary',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.accentSecondary,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Accent-Tertiary',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.accentTertiary,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Text',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.text,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Button-Text',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.buttonText,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Text-Disabled',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.textDisabled,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Background',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.background,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Background-Secondary',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.backgroundSecondary,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Success',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.success,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Error',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.error,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Disabled',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.disabled,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Transparent',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.transparent,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Gradient-First',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.gradientFirst,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Gradient-Second',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.gradientSecond,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Border',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.border,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Border-Gradient-First',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.borderGradientFirst,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Border-Gradient-Second',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.borderGradientSecond,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Border-Gradient-Second',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.borderGradientSecond,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Icon',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.icon,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Icon-Background',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.iconBackground,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Shadow',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.shadow,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Link',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.link,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Onboarding-Background-1',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.onboardingBackground1,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Onboarding-Background-2',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.onboardingBackground2,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Onboarding-Background-3',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.onboardingBackground3,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Menu-Card-Gradient-First',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.menuCardGradientFirst,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Menu-Card-Gradient-Second',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.menuCardGradientSecond,
+            ),
+          ),
+          GoldenTestScenario(
+            name: 'Menu-Card-Content',
+            child: _ColorTile(
+              color: UiKitTheme.darkThemeColors.menuCardContent,
             ),
           ),
         ],
